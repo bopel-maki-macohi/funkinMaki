78c01bda436d3aa2bf94017c9b542d23a50d7e55
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-24 18:16:19 -0500

    my own parsing system

--- source/ChartParser.hx
+++ source/ChartParser.hx
@@ -13,12 +13,21 @@ class ChartParser
 		var csvData = FlxStringUtil.imageToCSV(AssetHandler.baseAssetTmage('data/$songName/section_$section'));
 
 		var lines:Array<String> = regex.split(csvData);
-		var rows:Array<String> = lines.filter(function(line) return line != "");
+		var baseRows:Array<String> = lines.filter(function(line) return line != "");
+
+        var newRows:Array<String> = [];
+
+		for (row in baseRows)
+		{
+			row = row.trim().replace(', ', '.');
+
+            newRows.push(row);
+            trace('"$row"');
+		}
+
 		csvData.replace("\n", ',');
 
-		var heightInTiles = rows.length;
-		var widthInTiles = 8;
-
-        trace(rows);
+		var heightInTiles = newRows.length;
+		var widthInTiles = newRows[0].split('.').length;
 	}
 }
