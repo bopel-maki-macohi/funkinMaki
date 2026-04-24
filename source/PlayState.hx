7bd7aaa85ea5c6759fd517c7a2eb57ed7473ecd9
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-24 17:15:49 -0500

    add back dancing and music

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.FlxG;
+
 class PlayState extends ConductorState
 {
 	var bf:Character;
@@ -9,9 +11,17 @@ class PlayState extends ConductorState
 		super.create();
 
 		conductor.bpm = 140.0;
+		FlxG.sound.playMusic(AssetHandler.music('Test'), 1, false);
 
 		bf = new Character(0, 0);
 		bf.screenCenter();
 		add(bf);
+	}
+
+	override function beatHit(beat:Float)
+	{
+		super.beatHit(beat);
+
+		bf.dance();
 	}
 }
